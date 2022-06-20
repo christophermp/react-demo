878d9dc2946f08661ba873f8b42822f7312ab9ff
Christopher Meldre Pettersen <meldre@outlook.com>
2022-06-20 14:04:19 +0200

    Init

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,14 @@
+function MainNavigation() {
+  return (
+    <header>
+      <div>React Meetups</div>
+      <nav>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
