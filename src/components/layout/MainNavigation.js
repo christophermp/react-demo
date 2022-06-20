e340fefee3e87cb159f3cf39162d907e06f723a8
Christopher Meldre Pettersen <meldre@outlook.com>
2022-06-20 17:08:30 +0200

    Init

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,10 +1,20 @@
+import { Link } from "react-router-dom";
+
 function MainNavigation() {
   return (
     <header>
       <div>React Meetups</div>
       <nav>
         <ul>
-          <li></li>
+          <li>
+            <Link to={"/"}>All Meetups</Link>
+          </li>
+          <li>
+            <Link to={"/new-meetup"}>Add New Meetup</Link>
+          </li>
+          <li>
+            <Link to={"/favorites"}>My Favorites</Link>
+          </li>
         </ul>
       </nav>
     </header>
