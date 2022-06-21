fac777d82545f053d85dc8fba1a6cbafdefde307
Christopher Meldre Pettersen <meldre@outlook.com>
2022-06-21 13:37:24 +0200

    Init

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,17 +1,22 @@
+import classes from "./MeetupItem.module.css";
+import Card from "../ui/Card";
+
 function MeetupItem(props) {
   return (
-    <li>
-      <div>
-        <img src={props.image} alt={props.title} />
-      </div>
-      <div>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div>
-        <button>To Favorites</button>
-      </div>
+    <li className={classes.item}>
+      <Card>
+        <div>
+          <img src={props.image} alt={props.title} className={classes.image} />
+        </div>
+        <div className={classes.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button>To Favorites</button>
+        </div>
+      </Card>
     </li>
   );
 }
