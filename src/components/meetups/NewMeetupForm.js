029de813e9a2ac56321adcc2aaf4ed95451142b9
Christopher Meldre Pettersen <meldre@outlook.com>
2022-06-25 13:21:26 +0200

    Init

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,25 +1,69 @@
+import { useRef } from "react";
 import classes from "./NewMeetupForm.module.css";
 import Card from "../ui/Card";
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+
+    props.onAddMeetup(meetupData);
+  }
+
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor={"title"}>Meetup Title</label>
-          <input type={"text"} required={true} id={"title"} />
+          <input
+            type={"text"}
+            required={true}
+            id={"title"}
+            ref={titleInputRef}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor={"image"}>Meetup Image</label>
-          <input type={"url"} required={true} id={"image"} />
+          <input
+            type={"url"}
+            required={true}
+            id={"image"}
+            ref={imageInputRef}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor={"address"}>Address</label>
-          <input type={"text"} required={true} id={"address"} />
+          <input
+            type={"text"}
+            required={true}
+            id={"address"}
+            ref={addressInputRef}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor={"description"}>Description</label>
-          <textarea required={true} id={"description"} rows={"5"} />
+          <textarea
+            required={true}
+            id={"description"}
+            rows={"5"}
+            ref={descriptionInputRef}
+          />
         </div>
         <div className={classes.actions}>
           <button>Add Meetup</button>
